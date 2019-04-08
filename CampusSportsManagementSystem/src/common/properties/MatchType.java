d25dc39d9529df4d3e5db9df6aea4af7c9796770
zhouwhui <qc_d9bcefc35c9a49a0b26d7792f8993094@git.code.tencent.com>
2019-04-08 16:14:31 +0800

    属性值

---
+++ CampusSportsManagementSystem/src/common/properties/MatchType.java
@@ -0,0 +1,20 @@
+package common.properties;
+
+public class MatchType {
+	/**
+	 * 未开始
+	 */
+	public static final int NotStarted = 1;
+	/**
+	 * 已结束
+	 */
+	public static final int Finish = 2;
+	/**
+	 * 延时
+	 */
+	public static final int Delayed = 3;
+	/**
+	 * 取消
+	 */
+	public static final int Cancel = 4;
+}
