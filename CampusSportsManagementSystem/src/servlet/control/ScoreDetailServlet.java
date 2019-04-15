d52c230f7833ee07ade3e7266d116b2a295357bb
zhouwhui <qc_d9bcefc35c9a49a0b26d7792f8993094@git.code.tencent.com>
2019-04-15 21:12:44 +0800

    查看成绩详情

---
+++ CampusSportsManagementSystem/src/servlet/control/ScoreDetailServlet.java
@@ -0,0 +1,27 @@
+package servlet.control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ScoreDetailServlet
+ */
+@WebServlet("/scoredetail.do")
+public class ScoreDetailServlet extends HttpServlet {
+    public ScoreDetailServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
