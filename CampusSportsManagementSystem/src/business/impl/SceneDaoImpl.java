dfdfe5d5413c918e921597b93d6fab4aba794de8
mz <mz@DESKTOP-LUO88QN>
2019-04-16 11:10:13 +0800

    查看我的項目

--- CampusSportsManagementSystem/src/business/impl/SceneDaoImpl.java
+++ CampusSportsManagementSystem/src/business/impl/SceneDaoImpl.java
@@ -1,8 +1,10 @@
 package business.impl;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
+import model.Project;
 import model.Scene;
 import business.basic.BaseDao;
 import business.basic.BaseDaoImpl;
@@ -42,13 +44,35 @@ public class SceneDaoImpl implements SceneDAO {
 		list = Scene.toList(rs);
 		return list;
 	}
+	
+	@Override
+	public List<Project> seleOtherScenes(String userid) {
+		String sql = "SELECT proname from v_Match where userid=? except SELECT proname from V_Scene where userid=?";
+		Object[] para = {userid,userid};
+		ResultSet rs = bdao.select(sql,para);
+		List<Project> list = new ArrayList<Project>();
+		if(rs != null){
+			try{
+				while(rs.next()){
+					Project project = new Project();
+					project.setProname(rs.getString("proname"));
+					list.add(project);
+				}
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+	
 	public static void main(String[] args){
 		SceneDAO sDao = new SceneDaoImpl();
-		List<Scene> list = sDao.seleScenes("1001");
+		List<Project> list = sDao.seleOtherScenes("1001");
 		System.out.print(list.size());
 		if(list!=null){
-			for(Scene scene:list){
-				System.out.println(scene.getArrange().getProject().getProname());
+			for(Project scene:list){
+				System.out.println(scene.getProname());
 			}
 		}
 	}
