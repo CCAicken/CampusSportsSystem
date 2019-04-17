e232ede2a5b5c768c6b0ebd298d8ad2a9bf8608b
zhouwhui <qc_d9bcefc35c9a49a0b26d7792f8993094@git.code.tencent.com>
2019-04-17 08:33:28 +0800

    连接字符串

--- CampusSportsManagementSystem/src/business/basic/DBConnection.java
+++ CampusSportsManagementSystem/src/business/basic/DBConnection.java
@@ -13,8 +13,8 @@ import java.sql.Statement;
 public class DBConnection {
 
 	private String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	//private String url = "jdbc:sqlserver://192.168.3.60:1433;DatabaseName=CSMS";
-	private String url = "jdbc:sqlserver://localhost:1433;DatabaseName=CSMS";
+	private String url = "jdbc:sqlserver://192.168.3.60:1433;DatabaseName=CSMS";
+	//private String url = "jdbc:sqlserver://localhost:1433;DatabaseName=CSMS";
 	private String user = "sa";
 	private String pwd = "123456";
 	/**
